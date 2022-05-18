fcd73e7153c8f524c33e8160e1d2c790eb8c4871
Oleg Novikov <pnzdmd@yandex.ru>
2022-05-18 15:54:52 +0300

    Добавить фильтрацию и поиск

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,8 +1,13 @@
 import '../style/style.css'
 import { cart } from "./modules/cart";
-import second from "./modules/second"
+import load from './modules/load'
+import search from './modules/search'
+import catalog from './modules/catalog'
 
-
-second();
+load();
 
 cart();
+
+search();
+
+catalog()

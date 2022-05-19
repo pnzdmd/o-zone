4231b33b37b28474021937ea9c46299de9c257b9
Oleg Novikov <pnzdmd@yandex.ru>
2022-05-19 12:22:15 +0300

    Реализовать покупку и отправку товаров на сервер

--- src/pages/modules/cart.js
+++ src/pages/modules/cart.js
@@ -1,11 +1,30 @@
+import renderCart from "./renderCart";
+import postData from "./postData";
+
+
 export const cart = () => {
   const cartBtn = document.querySelector('#cart');
   const cartModal = document.querySelector('.cart');
   const cartCloseBtn = cartModal.querySelector('.cart-close');
+  const cartTotal = cartModal.querySelector('.cart-total > span');
+  const goodsWrapper = document.querySelector('.goods');
+  const cartWrapper = document.querySelector('.cart-wrapper');
+  const cartSendBtn = document.querySelector('.cart-confirm');
+  const cartCounter = document.querySelector('.counter')
+
 
   // открытие корзины
   const openCart = () => {
     cartModal.style.display = 'flex';
+    const cart = localStorage.getItem('cart') ?
+      JSON.parse(localStorage.getItem('cart')) : [];
+
+    renderCart(cart)
+    cartTotal.textContent = cart.reduce((sum, goodItem) => {
+      return sum + goodItem.price
+    }, 0)
+
+
   }
   // закрытие корзины
   const closeCart = () => {
@@ -14,4 +33,65 @@ export const cart = () => {
 
   cartBtn.addEventListener('click', openCart);
   cartCloseBtn.addEventListener('click', closeCart);
+
+  // добавление товара в корзину
+  goodsWrapper.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn-primary')) {
+      const card = e.target.closest('.card');
+      const key = card.dataset.key;
+
+      const goods = JSON.parse(localStorage.getItem('goods'));
+      const cart = localStorage.getItem('cart') ?
+        JSON.parse(localStorage.getItem('cart')) : [];
+
+      const goodItem = goods.find((item) => {
+        return item.id === +key
+      })
+
+      cart.push(goodItem)
+      localStorage.setItem('cart', JSON.stringify(cart))
+
+      cartCounter.textContent = cart.length;
+
+    }
+  })
+  // удаление товара из корзины
+  cartWrapper.addEventListener('click', (e) => {
+    if (e.target.classList.contains('btn-primary')) {
+      const cart = localStorage.getItem('cart') ?
+        JSON.parse(localStorage.getItem('cart')) : [];
+
+      cartCounter.textContent = cart.length - 1;
+
+      const card = e.target.closest('.card');
+      const key = card.dataset.key;
+
+      const index = cart.findIndex((item) => {
+        return item.id === +key;
+      })
+
+      cart.splice(index, 1);
+
+      localStorage.setItem('cart', JSON.stringify(cart))
+
+      renderCart(cart)
+      cartTotal.textContent = cart.reduce((sum, goodItem) => {
+        return sum + goodItem.price
+      }, 0)
+    }
+  })
+  // отправка данных о товаре из корзины
+  cartSendBtn.addEventListener('click', () => {
+    const cart = localStorage.getItem('cart') ?
+      JSON.parse(localStorage.getItem('cart')) : [];
+
+    postData(cart).then(() => {
+      // удаление товара и очистка корзины после отправки данных
+      localStorage.removeItem('cart');
+
+      renderCart([]);
+      cartTotal.textContent = 0;
+    })
+    cartCounter.textContent = 0;
+  })
 }
--- src/pages/modules/postData.js
+++ src/pages/modules/postData.js
@@ -1,16 +1,12 @@
-const postData = () => {
-    return fetch('https://jsonplaceholder.typicode.com/posts', {
-        method: 'POST',
-        body: JSON.stringify({
-            title: 'foo',
-            body: 'bar',
-            userId: 1,
-        }),
-        headers: {
-            'Content-type': 'application/json; charset=UTF-8',
-        },
-    })
-        .then((res) => res.json())
+const postData = (cart) => {
+  return fetch('https://jsonplaceholder.typicode.com/posts', {
+    method: 'POST',
+    body: JSON.stringify(cart),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  })
+    .then((res) => res.json())
 
 }
 
